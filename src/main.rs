92dda586b0f23e72bf41c9d9343318e1423fbc59
Lukas Beierlieb <lukas.beierlieb@uni-wuerzburg.de>
2024-03-27 00:46:05 +0100

    Generate html report and open in browser

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
+use maud::html;
 use reqwest::blocking::get;
 use std::fs;
 
 use scraper::{selectable::Selectable, Html, Selector};
 
 fn main() {
-    let list_document = Html::parse_document(
-        &fs::read_to_string("example_html_files/competitors_list.html").unwrap(),
-    );
-    dbg!(get_competitors_and_profiles(&list_document));
+    //let list_document = Html::parse_document(
+    //    &fs::read_to_string("example_html_files/competitors_list.html").unwrap(),
+    //);
+    //dbg!(get_competitors_and_profiles(&list_document));
 
-    let competitor_document = Html::parse_document(
-        &fs::read_to_string("example_html_files/single_competitor.html").unwrap(),
-    );
-    dbg!(get_best_3x3_avg_from_html(&competitor_document));
-    dbg!(get_best_3x3_avg("/persons/2023BEIE01"));
-    for (name, time) in get_competitors_and_times(
+    //let competitor_document = Html::parse_document(
+    //    &fs::read_to_string("example_html_files/single_competitor.html").unwrap(),
+    //);
+    //dbg!(get_best_3x3_avg_from_html(&competitor_document));
+    //dbg!(get_best_3x3_avg("/persons/2023BEIE01"));
+    let competitor_data = get_competitors_and_times(
         "https://www.worldcubeassociation.org/competitions/HessenMiniOpen2024/registrations",
-    ) {
-        if let Some(t) = time {
-            println!("{}: {}", name, t);
-        } else {
-            println!("{}", name);
+    );
+    let path = "test.html";
+    let report = generate_report(competitor_data);
+
+    if let Err(err) = fs::write(path, report) {
+        eprintln!("Error writing html file: {:?}", err);
+    } else {
+        if let Err(err) = webbrowser::open(path) {
+            eprintln!("Error opening browser: {:?}", err);
         }
     }
 }
@@ -68,4 +73,15 @@ fn get_best_3x3_avg_from_html(competitor_html: &Html) -> Option<String> {
         .select(&selector)
         .next()
         .map(|element| element.text().collect::<String>().trim().to_owned())
+}
+
+fn generate_report(competitor_data: Vec<(String, Option<String>)>) -> String {
+    let markup = html! {
+        ul {
+            @for (name, maybe_time) in &competitor_data {
+                li { (name) (match maybe_time { Some(time) => format!(": {}", time), None => "".to_string()}) }
+            }
+        }
+    };
+    markup.into_string()
 }
