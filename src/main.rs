69e359688a2fd343aed446da76242a600c3738e3
Lukas Beierlieb <lukas.beierlieb@uni-wuerzburg.de>
2024-03-27 23:25:20 +0100

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use maud::html;
 use reqwest::blocking::get;
 use std::fs;
+use thiserror::Error;
 
 use scraper::{selectable::Selectable, Html, Selector};
 
@@ -20,63 +21,42 @@ impl Competitor {
     }
 }
 
-fn main() {
-    //let list_document = Html::parse_document(
-    //    &fs::read_to_string("example_html_files/competitors_list.html").unwrap(),
-    //);
-    //dbg!(get_competitors_and_profiles(&list_document));
-
-    //let competitor_document = Html::parse_document(
-    //    &fs::read_to_string("example_html_files/single_competitor.html").unwrap(),
-    //);
-    //dbg!(get_best_3x3_avg_from_html(&competitor_document));
-    //dbg!(get_best_3x3_avg("/persons/2023BEIE01"));
-    let competitor_data = get_competitors_and_times(
-        "https://www.worldcubeassociation.org/competitions/HessenMiniOpen2024/registrations",
-    );
-    //let competitor_data = vec![
-    //    Competitor::new("Hannah".into(), None),
-    //    Competitor::new("Hannah Otto".into(), Some("profile".into())),
-    //];
+fn main() -> Result<(), WCOError> {
+    let url = "https://www.worldcubeassociation.org/competitions/HessenMiniOpen2024/registrations";
     let path = "test.html";
-    let report = generate_report(&competitor_data);
 
-    if let Err(err) = fs::write(path, report) {
-        eprintln!("Error writing html file: {:?}", err);
-    } else {
-        if let Err(err) = webbrowser::open(path) {
-            eprintln!("Error opening browser: {:?}", err);
-        }
+    generate_report(&url, &path)?;
+    webbrowser::open(path)?;
+    Ok(())
+}
+
+fn generate_report(competitors_url: &str, out_path: &str) -> Result<(), WCOError> {
+    let competitors_html = Html::parse_document(&get(competitors_url)?.text()?);
+    let competition_title = get_competition_title(&competitors_html)?;
+    let mut competitors = parse_competitors(&competitors_html);
+    retrieve_competitor_pr_avgs(&mut competitors)?;
+    let report = generate_report_html(&competition_title, &competitors);
+    fs::write(out_path, report)?;
+    Ok(())
+}
+
+fn get_competition_title(html: &Html) -> Result<String, WCOError> {
+    let selector = Selector::parse(r#"h3"#).expect("Parsing known selector should not fail");
+
+    let h3_headlines: Vec<_> = html.select(&selector).collect();
+    match h3_headlines.len() {
+        1 => Ok(h3_headlines[0].text().collect::<String>().trim().to_owned()),
+        len => Err(WCOError::ParsingError(format!(
+            "Expected competition title to be the only h3 element, but there were {}",
+            len
+        ))),
     }
 }
 
-fn get_competition_title(html: &Html) -> Option<String> {
-    let selector = Selector::parse(r#"h3"#).unwrap();
-
-    html.select(&selector)
-        .next()
-        .map(|elem| elem.text().collect::<String>().trim().to_owned())
-}
-
-fn get_competitors_and_times(url: &str) -> Vec<Competitor> {
-    let html = Html::parse_document(&get(url).unwrap().text().unwrap());
-    get_competitors_and_times_from_html(&html)
-}
-
-fn get_competitors_and_times_from_html(competitors_list: &Html) -> Vec<Competitor> {
-    let mut competitors = get_competitors_and_profiles(competitors_list);
-    for competitor in &mut competitors {
-        competitor.pr_3x3_avg = competitor
-            .profile
-            .clone()
-            .and_then(|profile| get_best_3x3_avg(&profile));
-    }
-    competitors
-}
-
-fn get_competitors_and_profiles(competitors_list: &Html) -> Vec<Competitor> {
-    let selector = Selector::parse(r#"td[class="name"]"#).unwrap();
-    let name_selector = &Selector::parse("a").unwrap();
+fn parse_competitors(competitors_list: &Html) -> Vec<Competitor> {
+    let selector =
+        Selector::parse(r#"td[class="name"]"#).expect("Parsing known selector should not fail");
+    let name_selector = &Selector::parse("a").expect("Parsing known selector should not fail");
 
     competitors_list
         .select(&selector)
@@ -92,14 +72,20 @@ fn get_competitors_and_profiles(competitors_list: &Html) -> Vec<Competitor> {
         .collect()
 }
 
-fn get_best_3x3_avg(profile: &str) -> Option<String> {
-    let url = format!("https://www.worldcubeassociation.org/{}", profile);
-    let html = Html::parse_document(&get(url).unwrap().text().unwrap());
-    get_best_3x3_avg_from_html(&html)
+fn retrieve_competitor_pr_avgs(competitors: &mut [Competitor]) -> Result<(), WCOError> {
+    for competitor in competitors {
+        if let Some(profile) = &mut competitor.profile {
+            let url = format!("https://www.worldcubeassociation.org/{}", profile);
+            let html = Html::parse_document(&get(url)?.text()?);
+            competitor.pr_3x3_avg = parse_pr_3x3_avg(&html);
+        }
+    }
+    Ok(())
 }
 
-fn get_best_3x3_avg_from_html(competitor_html: &Html) -> Option<String> {
-    let selector = Selector::parse(r#"a[href="/results/rankings/333/average"]"#).unwrap();
+fn parse_pr_3x3_avg(competitor_html: &Html) -> Option<String> {
+    let selector = Selector::parse(r#"a[href="/results/rankings/333/average"]"#)
+        .expect("Parsing known selector should not fail");
 
     competitor_html
         .select(&selector)
@@ -107,16 +93,17 @@ fn get_best_3x3_avg_from_html(competitor_html: &Html) -> Option<String> {
         .map(|element| element.text().collect::<String>().trim().to_owned())
 }
 
-fn generate_report(competitor_data: &[Competitor]) -> String {
+fn generate_report_html(competition_title: &str, competitor_data: &[Competitor]) -> String {
     let markup = html! {
         html {
             head {
                 title { "My Table" }
-
-                // Include the link to your CSS file
                 link rel="stylesheet" type="text/css" href="styles.css" {}
             }
             body {
+                h1 {
+                    (competition_title)
+                }
                 table {
                     @for competitor in competitor_data {
                         tr {
@@ -137,4 +124,16 @@ fn generate_report(competitor_data: &[Competitor]) -> String {
         }
     };
     markup.into_string()
+}
+
+#[derive(Error, Debug)]
+pub enum WCOError {
+    #[error("Invalid input: {0}")]
+    ParsingError(String),
+
+    #[error("I/O error: {0}")]
+    IOError(#[from] std::io::Error),
+
+    #[error("Network error: {0}")]
+    ReqwestError(#[from] reqwest::Error),
 }
